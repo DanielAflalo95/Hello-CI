374016ff0165c63aca88f0949f65c6c9a9f94d2a
DanielAflalo95 <Daniel1761995@gmail.com>
2025-07-27 22:12:06 +0300

    Add GitHub Actions CI workflow

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    print("Welcome to my first CI project! My name is Daniel")
+    print("Welcome to my first CI project!")
     print(f"2 + 3 = {add(2, 3)}")
